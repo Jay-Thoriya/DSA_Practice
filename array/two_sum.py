f8c9cad4fcf43c812e4a7ecb2d4991515fe9a731
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2024-06-07 09:39:48 +0530

    create a solution for given problem in O(n^2) complexity

--- array/two_sum.py
+++ array/two_sum.py
@@ -1,1 +1,24 @@
-#
+# Input: arr[] = {0, -1, 2, -3, 1}, x= -2
+# Output: Yes
+# Explanation: If we calculate the sum of the output,1 + (-3) = -2
+
+# Input: arr[] = {1, -2, 1, 0, 5}, x = 0
+# Output: No
+
+def chkPair(a, x):
+    for i in range(0,len(a)):
+        for j in range(i,len(a)):
+            if(a[i]+a[j] == x and i!=j):
+                return 1
+                break
+
+a1 = [0, -1, 2, -3, 1]
+x1 = -2
+a2 = [1, -2, 1, 0, 5]
+x2 = 0
+if chkPair(a2,x2) == 1: 
+    print("yes")
+else:
+    print("no")
+
+
